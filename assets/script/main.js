a6f0fd409bf04dd172c78f3f3e16fc0099efb376
Misato-0112 <misato@m0112.mygbiz.com>
2024-10-04 23:33:09 -0500

    10.05提出

---
+++ assets/script/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    // Slickスライダー
+    $(".voices-slider__container").slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+
+        prevArrow: $(".voices-slider__button--prev"),
+        nextArrow: $(".voices-slider__button--next"),
+        dotsClass: "slide-dots",
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    // アコーディオン
+    $(".accordion__title").click(function () {
+        $(this).toggleClass("accordion__title--active");
+
+        $(this).next(".accordion__content").slideToggle(300);
+
+        $(".accordion__content").not($(this).next()).slideUp(300);
+        $(".accordion__title").not($(this)).removeClass("accordion__title--active");
+    });
+
+    // Topへ戻るボタン
+    var backToTopButton = $(".back-to-top");
+    var showButtonHeight = 200;
+    var fadeDuration = 800;
+
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > showButtonHeight) {
+            backToTopButton.addClass("show fade-in-down").removeClass("fade-in-up");
+        } else {
+            backToTopButton.addClass("fade-in-up").removeClass("show fade-in-down");
+        }
+    });
+
+    backToTopButton.click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 300);
+    });
+});
